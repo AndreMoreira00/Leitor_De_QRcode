1407cf2664feb4eca47839758e161bb151e5b903
AndreMoreira00 <andremoreira102030@gmail.com>
2024-05-20 17:08:42 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { Text, StatusBar, Pressable } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import styles from "./AppStyle";
 
 export default function App() {
+
+  const [escaneado, estadoEscaneado]= useState(false)
+
+  async function ObterPermissao(){
+    await BarCodeScanner.requestPermissionsAsync()
+  }
+
+  useEffect(function(){
+    ObterPermissao()
+  }, [])
+
+  function Escanear({type, data}){
+    estadoEscaneado(true)
+    alert(data)
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LinearGradient style={styles.tela} colors={["#232526", "#414345"]}>
+      <StatusBar 
+      translucent
+      barStyle={"light-content"}
+      backgroundColor={"transparent"}/>
+
+      {escaneado ?
+        <Pressable onPress={function(){estadoEscaneado(false)}}>
+          <Text style={styles.texto}>Escanear novamente</Text>
+        </Pressable>
+        :
+        <BarCodeScanner style={styles.camera} onBarCodeScanned={Escanear}/>
+      }
+    </LinearGradient>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
